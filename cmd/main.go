4a0e568c82f14761ab453b3d60305209965e9349
hyperion9913 <supernoob7903@gmail.com>
2025-05-07 17:48:24 +0530

    completed conditionals

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,35 @@
 package main
-import "fmt"
 
-func main(){
-	ver1 := `"Hello 
-I am Hyperion"`
+import(
+	"fmt"
+	"errors"
+)
 
-	fmt.Println(ver1)
+func main() {
+	var value string = "Hello another function"
+	anotherFunction(value)
 
-	fmt.Println("Hello")
+	var num int = 10
+	var den int = 0
+	var result, err = intDivision(num, den)
+	switch result{
+	case 0:
+		 fmt.Println(err.Error())
+	default: 
+			fmt.Printf("\nResult is : %v",result)
+	}
+}
+
+func anotherFunction(value string) {
+	fmt.Println(value)
+}
+
+func intDivision(num int, den int) (int, error) {
+	var err error
+	if den == 0 {
+		err = errors.New("\nError : Denominator Is Zero")
+		return 0, err
+	}
+	var result int = num/den
+	return result, err
 }
