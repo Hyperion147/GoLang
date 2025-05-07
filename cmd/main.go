12ab5b4184941f5ea4b935cbabcdca77b5af4a39
hyperion9913 <supernoob7903@gmail.com>
2025-05-07 17:00:15 +0530

    golang

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main(){
+	ver1 := `"Hello 
+I am Hyperion"`
+
+	fmt.Println(ver1)
+
+	fmt.Println("Hello")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hyperion147/go_basic
+
+go 1.24.3
