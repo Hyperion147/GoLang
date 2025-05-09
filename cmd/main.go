bd4ba8e628f9a70c706fcae1ae0fddc0434d6a04
hyperion9913 <supernoob7903@gmail.com>
2025-05-09 16:24:19 +0530

    need to understand channels deeper

--- cmd/main.go
+++ cmd/main.go
@@ -1,43 +1,13 @@
 package main
 
-import ( 
-	"fmt" 
-	"sync"	
-	"time"
-)
-
-var m = sync.RWMutex{}
-var wg = sync.WaitGroup{}
-var data = []string{"lesgo", "lesgoo", "lesgooo"}
-var result = []string{}
+import "fmt"
 
 func main(){
-	t0 := time.Now()
-	for i:= 0; i<len(data);i++{
-		wg.Add(1)
-		go dataCall(i)
-	}
-	wg.Wait()
-	fmt.Printf("\nTotal Execution time: %v", time.Since(t0))
-	fmt.Printf("\nThe results are: %v", result)
+	var i = make(chan int)
+	go printingChannel(i)
+	fmt.Println(<-i)
 }
 
-func dataCall(i int){
-	var delay float32 = 2000
-	time.Sleep(time.Duration(delay)*time.Millisecond)
-	save(data[i])
-	log()
-	wg.Done()
-}
-
-func save(results string){
-	m.Lock()
-	result = append(result, results)
-	m.Unlock()
-}
-
-func log(){
-	m.RLock()
-	fmt.Printf("\nThe current results are: %v", result)
-	m.RUnlock()
+func printingChannel(i chan int){
+	i <- 110
 }
