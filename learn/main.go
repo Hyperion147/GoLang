bf1f7b09a9f79b085c0a16914325dd8a1991f3ac
Hyperion147 <supernoob7903@gmail.com>
2025-07-10 16:02:00 +0530

    go strings

--- learn/main.go
+++ learn/main.go
@@ -1,9 +1,25 @@
 package main
 
-import(
+import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func main(){
-	fmt.Println("Hello Hyperion Nice Custom terminal")
+func main() {
+	welcome := "This is a simple input for getting discord username"
+	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your discord username:")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Your username is:", input)
+	increment, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("The incremented name is:", increment + 1)
+	}
 }
